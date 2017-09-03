b7916fe92f71a808a3e71c7013e2234148035c3d
wlx10011101 <810635487@qq.com>
2017-09-03 19:52:46 +0800

    增加websocket
    增加wensocket内容，play内增加服务端，main增加模拟客户端

--- WebSocket-Server-py/Main.py
+++ WebSocket-Server-py/Main.py
@@ -5,26 +5,23 @@ Created on Sep 2, 2017
 
 @author: wlx
 '''
-from bottle import route
-from bottle import run
-import bottle
-from bottle_websocket.plugin import websocket
-from bottle_websocket.server import GeventWebSocketServer
+import os
+import sys
 
+from websocket._core import create_connection
 
-@route('/websocket', apply=[websocket])
-def echo(ws):
-    while True:
-        msg = ws.receive()
-        if msg is not None:
-            ws.send(msg)
-        else:
-            break
-
-
-@route('/')
-def ret():
-    return "welcome websocket server"
 
 if __name__ == "__main__":
-    run(host='127.0.0.1', port=8203, server=GeventWebSocketServer)
+    ws1 = create_connection("ws://127.0.0.1:3035/websocket")
+    ws2 = create_connection("ws://127.0.0.1:3035/websocket")
+    print "start"
+    ws2.send("start hello world")
+    ws1.send("ws1- start hellpo world")
+    print "receiving..."
+    result2 = ws2.recv()
+    result1 = ws1.recv()
+    print "result", result2
+    print "result1", result1
+    os.system("pause")
+#     ws2.close()
+#     ws1.close()
--- WebSocket-Server-py/Server/play.py
+++ WebSocket-Server-py/Server/play.py
@@ -5,12 +5,17 @@ Created on Sep 2, 2017
 
 @author: wlx
 '''
-from bottle import route, run, request, response
-import bottle
+
+
 import json
 import logging
 
+from bottle import route, run, request, response
+import bottle
+from bottle_websocket.plugin import websocket
+
 from customer.data import shapeData
+from bottle_websocket.server import GeventWebSocketServer
 
 
 shapeDataHandler = shapeData()
@@ -20,6 +25,19 @@ shapeDataHandler = shapeData()
 def getShapeData():
     response.content_type = 'application/json; charset=utf-8'
     return json.dumps(shapeDataHandler.get(int(request.params.get('id'))))
+
+
+@route('/websocket', apply=[websocket])
+def communication(ws):
+    while True:
+        msg = ws.receive()
+        print dir(msg)
+        print "11", msg
+        if msg is not None:
+            ws.send(msg)
+        else:
+            #             break
+            print msg
 
 
 @route('/')
@@ -37,4 +55,4 @@ def static_js_files(filename):
     return bottle.static_file(filename, root='../public/js/')
 
 if __name__ == "__main__":
-    run(host='0.0.0.0', port=3035, debug=logging.DEBUG)
+    run(host='0.0.0.0', port=3035, debug=logging.DEBUG, server=GeventWebSocketServer)
